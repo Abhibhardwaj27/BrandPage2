9a3ac88ad3569c09c018116eb080f4606e1b2f53
Abhi-LM10 <abhi.bhardwaj22@st.niituniversity.in>
2025-05-23 12:47:14 +0530

    till updated files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <nav>
+          <div className='logo'>
+            <img src="/images/brand_logo.png" alt="logo" />
+          </div>
+          <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+          </ul>
+          <button>Login</button>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
